0c2e499aa0f404debcc84d4957a97e86fd56f625
Thulasiviranchan06 <128715271+Thulasiviranchan06@users.noreply.github.com>
2023-06-02 17:25:55 +0530

    Add files via upload

---
+++ TestWorking.java
@@ -0,0 +1,7 @@
+public class TestWorking{
+public static void main(String args[]){
+System.out.print("hii.how ru");
+WORKING ob=new WORKING();
+ob.work();
+}
+}
---
+++ WORKING.java
@@ -0,0 +1,5 @@
+public class WORKING{
+public void work(){
+System.out.print("hello,Good morning");
+}
+}
