c925a10216126b17e8150dfc4f56b4f33be846db
Thulasiviranchan06 <128715271+Thulasiviranchan06@users.noreply.github.com>
2023-06-02 17:55:34 +0530

    Add files via upload

---
+++ Program208.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Program208
+{
+public static void main(String args[])
+{
+char ch;
+Scanner sc= new Scanner(System.in);
+ch=sc.next().charAt(0);
+int x=(int)ch;
+System.out.println("ASCII VALUE OF:"+ch+"is"+x);
+}
+}
+
